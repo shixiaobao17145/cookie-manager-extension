52e8c24e01c7ba450f89984ac63de7e71ebb74c4
Bob <shixiaobao17145@163.com>
2017-07-10 10:27:53 +0800

    add publish stuff

---
+++ publish/webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+module.exports = {
+	devtool: 'source-map',
+	entry: {
+		manager:"./manager.entry.js"
+	},
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: "[name].bundle.js"
+	},
+	module: {
+		loaders: [
+			{ test: /\.css$/, loader: "style-loader!css-loader" },
+            {
+                test: /.js?$/, loader: 'babel-loader', exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react', 'stage-0'],
+                    //plugins: ['transform-decorators-legacy']
+                }
+            }
+		]
+	}
+};
